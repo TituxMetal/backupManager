2b4502bcdc258bb421e7c5fff9533f43ac0adff8
Titux Metal (DEV) <github@lgdweb.fr>
2024-05-26 17:47:20 +0200

    feat: improve the code by adding some functions for repeated task, adding global variable and add basic documentation to each functions

--- backupManager.sh
+++ backupManager.sh
@@ -10,7 +10,13 @@
 
 # Global variables
 TEST_MODE=false
+BASE_DIR=./testEnv
+SERVERS=(andrew bart lisa laura mary homelab)
 
+# Function to print a message with formatting
+# Usage: printMessage <message>
+# Arguments:
+#   - message: The message to be printed
 printMessage() {
   local message="$1"
   if [ -t 1 ]; then  # Check if output is a terminal
@@ -26,6 +32,11 @@ printMessage() {
   fi
 }
 
+# Function to handle errors and exit the script if an error occurs.
+# Parameters:
+#   None
+# Returns:
+#   None
 handleError() {
   local exitCode=$?
   if [ $exitCode -ne 0 ]; then
@@ -34,64 +45,78 @@ handleError() {
   fi
 }
 
+# Function to create a directory.
+# Usage: createDir <directory_path>
+#   <directory_path>: The path of the directory to be created.
+createDir() {
+  mkdir -pv "$1"
+}
+
+# Function to populate a directory with sample data
+# Arguments:
+#   $1: The name of the data
+#   $2: The file to write the sample data to
+populateDir() {
+  local data="$1"
+  local file="$2"
+  echo "Sample data for $data" > "$file"
+}
+
+# Function to set up the test environment for the backupManager script.
+# This function creates the necessary source and destination directories for each server,
+# as well as populates them with sample files for testing purposes.
+# It also creates the source and destination directories for backupAllDataZ and backupVirtManager,
+# and populates them with sample files.
+# Finally, it creates the destination directory for syncBackups.
+# This function does not return any value.
 setupTestEnvironment() {
   printMessage "Setting up test environment..."
 
-  # Create source test directories for backupServers
-  mkdir -pv ~/test_env/{andrew,bart,lisa,laura,mary,homelab}/{home,srv,etc,var/lib/docker/volumes,var/log}
-  #  Create destination test directories for backupServers
-  mkdir -pv ~/test_env/media/local/Backup--timeshift/remote/{andrew,bart,lisa,laura,mary,homelab}
-  #  Create source test directory for backupAllDataZ
-  mkdir -pv ~/test_env/media/local/AllDataZ--Local
-  #  Create destination test directory for backupAllDataZ
-  mkdir -pv ~/test_env/homelab/media/local/AllDataZ
-  mkdir -pv ~/test_env/homelab/media/local/AllDataZ/toBeDeleted
-  #  Create source test direcory for backupVirtManager
-  mkdir -pv ~/test_env/home/titux/virt-manager
-  #  Create destination test directory for backupVirtManager
-  mkdir -pv ~/test_env/media/local/Backup--timeshift/virt-manager
-#  mkdir -pv ~/test_env/media/local/Backup--timeshift/virt-manager/{iso,disk}
-  #  Create destination test directory for syncBackups
-  mkdir -pv ~/test_env/homelab/media/local/backup
+  local serverDirs=("home" "srv" "etc" "var/lib/docker/volumes" "var/log")
 
+  # Create source and destination directories for each server
+  for server in ${SERVERS[@]}; do
+    for dir in ${serverDirs[@]}; do
+      # Create source test directories for backupServers
+      createDir "$BASE_DIR/$server/$dir"
+      # Create destination test directories for backupServers
+      createDir "$BASE_DIR/media/local/Backup--timeshift/remote/$server/$dir"
+      populateDir "/$server" "$BASE_DIR/$server/$dir/sample.txt"
+    done
 
-  printMessage "Populate test directories for servers with sample data"
-  for server in andrew bart lisa laura mary homelab; do
-    echo "Sample data for /home" > ~/test_env/$server/home/sample.txt
-    echo "Sample data for /srv" > ~/test_env/$server/srv/sample.txt
-    echo "Sample data for /etc" > ~/test_env/$server/etc/sample.txt
-    echo "Sample data for /var/lib/docker/volumes" > ~/test_env/$server/var/lib/docker/volumes/sample.txt
-    echo "Sample data for /var/log" > ~/test_env/$server/var/log/sample.txt
   done
-  
-  printMessage "Populate test directory for AllDataZ--Local with sample data"
-  
-  echo "Sample data for /AllDataZ--Local" > ~/test_env/media/local/AllDataZ--Local/sample.txt
-  
-  printMessage "Populate test data for AllDataZ with data to be deleted on dest"
-  
-  echo "Sample data to be deleted" > ~/test_env/homelab/media/local/AllDataZ/sample1.txt
-  touch ~/test_env/homelab/media/local/AllDataZ/toBeDeleted/deleteMe
-  
-  printMessage "Populate test direcectory for virt-manager with sample data"
 
-  echo "Sample data for /virt-manager" > ~/test_env/home/titux/virt-manager/sample.txt
-#  touch ~/test_env/media/local/Backup--timeshift/virt-manager/{iso,disk}/deleteMe
+  # Create source test directory for backupAllDataZ
+  createDir "$BASE_DIR/media/local/AllDataZ--Local"
+  # Create destination test directory for backupAllDataZ
+  createDir "$BASE_DIR/homelab/media/local/AllDataZ"
+  createDir "$BASE_DIR/homelab/media/local/AllDataZ/toBeDeleted"
+  populateDir "/AllDataZ--Local" "$BASE_DIR/media/local/AllDataZ--Local/sample.txt"
+  # Create source test direcory for backupVirtManager
+  createDir "$BASE_DIR/home/titux/virt-manager"
+  # Create destination test directory for backupVirtManager
+  createDir "$BASE_DIR/media/local/Backup--timeshift/virt-manager"
+  
+  populateDir "/virt-manager" "$BASE_DIR/home/titux/virt-manager/sample.txt"
+  # Create destination test directory for syncBackups
+  createDir "$BASE_DIR/homelab/media/local/backup"
+
 
   printMessage "Test environment setup completed."
 }
 
+# Function to backup all data from localAllDataZ to remoteHomelabAllDataZ using rsync.
 backupAllDataZ() {
   local localAllDataZ="/media/local/AllDataZ--Local"
   local remoteHomelabAllDataZ="supertux@homelab.local:/media/local/AllDataZ"
   local rsyncArgs=(-rltv --human-readable --progress --stats --delete --exclude='lost+found' --exclude='/**/*.cache/' --exclude='/**/*.next/' --exclude='/**/dist/' --exclude='/**/node_modules/')
-#  local rsyncArgs=(-rltv --dry-run --human-readable --progress --stats --delete --exclude='lost+found' --exclude='/**/*.cache/' --exclude='/**/*.next/' --exclude='/**/dist/' --exclude='/**/node_modules/')
-  
-
 
   if [ "$TEST_MODE" = true ]; then
-    localAllDataZ="$HOME/test_env/media/local/AllDataZ--Local"
-    remoteHomelabAllDataZ="$HOME/test_env/homelab/media/local/AllDataZ"
+    localAllDataZ="$BASE_DIR/media/local/AllDataZ--Local"
+    remoteHomelabAllDataZ="$BASE_DIR/homelab/media/local/AllDataZ"
+
+    printMessage "Source in test mode check: $localAllDataZ"
+    printMessage "Dest in test mode check: $remoteHomelabAllDataZ"
   fi
 
   rsync "${rsyncArgs[@]}" "$localAllDataZ/" "$remoteHomelabAllDataZ/"
@@ -99,32 +124,49 @@ backupAllDataZ() {
   printMessage "Backup of AllDataZ completed successfully."
 }
 
+# Function to backup the virt-manager directory using rsync.
 backupVirtManager() {
+  # Directory path of the virt-manager directory
   local virtManagerDir="/home/titux/virt-manager"
+  # Directory path where the backup will be stored
   local backupDir="/media/local/Backup--timeshift"
+  # Arguments for the rsync command
   local rsyncArgs=(-rltv --human-readable --progress --stats --delete --exclude='lost+found')
-#  local rsyncArgs=(-rltv --dry-run --human-readable --progress --stats --delete --exclude='lost+found')
 
   if [ "$TEST_MODE" = true ]; then
-    virtManagerDir="$HOME/test_env/home/titux/virt-manager"
-    backupDir="$HOME/test_env/media/local/Backup--timeshift/virt-manager"
+    # Directory path of the virt-manager directory in test mode
+    virtManagerDir="$BASE_DIR/home/titux/virt-manager"
+    # Directory path where the backup will be stored in test mode
+    backupDir="$BASE_DIR/media/local/Backup--timeshift/virt-manager"
+
+    printMessage "Source in test mode check: $virtManagerDir"
+    printMessage "Dest in test mode check: $backupDir"
   fi
 
+  # Perform the backup using rsync command
   rsync "${rsyncArgs[@]}" "$virtManagerDir/" "$backupDir/"
 
   printMessage "Backup of virt-manager completed successfully."
 }
 
+# Function to backup servers
+# Parameters:
+#   - serverName: The name of the server to backup
+# Description:
+#   This function performs a backup of specified directories on a remote server using rsync.
+#   It creates the necessary directory structure on the backup destination and then performs
+#   the rsync operation for each directory. If TEST_MODE is enabled, it uses a local directory
+#   as the source and destination for testing purposes.
+#   The function prints a success or failure message for each directory backup.
 backupServers() {
   local serverName="$1"
   local fullServerName="supertux@${serverName}.lgdweb.ovh:"
   local backupBaseDir="/media/local/Backup--timeshift/remote/${serverName}"
   local rsyncArgs=(-rltv --human-readable --progress --stats --delete --exclude='/lost+found' --exclude '/**/system@*' --exclude '/**/letsencrypt' --exclude '/**/dummykey.pem')
-  #  local rsyncArgs=(-rltv --dry-run --human-readable --progress --stats --delete --exclude='/lost+found' --exclude '/**/system@*' --exclude '/**/letsencrypt' --exclude '/**/dummykey.pem')
 
   if [ "$TEST_MODE" = true ]; then
-    fullServerName="$HOME/test_env/$serverName"
-    backupBaseDir="$HOME/test_env/media/local/Backup--timeshift/remote/${serverName}"
+    fullServerName="$BASE_DIR/$serverName"
+    backupBaseDir="$BASE_DIR/media/local/Backup--timeshift/remote/${serverName}"
 
     printMessage "Source in test mode check: $fullServerName"
     printMessage "Dest in test mode check: $backupBaseDir"
@@ -144,12 +186,14 @@ backupServers() {
       printMessage "Source in for loop: $fullServerName$dir/"
       printMessage "Dest in for loop: $backupBaseDir$dir/"
     fi
+
     rsync -e "ssh" "${rsyncArgs[@]}" --rsync-path="sudo rsync" "$fullServerName$dir/" "$backupBaseDir$dir/" \
     && printMessage "Backup of $dir from $serverName completed successfully." \
     || printMessage "Failed to backup $dir from $serverName."
   done
 }
 
+# Function to synchronize backups from a local directory to a remote directory.
 syncBackups() {
   local localBackupDir="/media/local/Backup--timeshift"
   local remoteHomelabBackupDir="supertux@homelab.local:/media/local/backup"
@@ -157,8 +201,11 @@ syncBackups() {
   #  local rsyncArgs=(-rltv --dry-run --human-readable --progress --stats --delete --exclude='lost+found' --exclude '/timeshift')
 
   if [ "$TEST_MODE" = true ]; then
-    localBackupDir="$HOME/test_env/media/local/Backup--timeshift"
-    remoteHomelabBackupDir="$HOME/test_env/homelab/media/local/backup"
+    localBackupDir="$BASE_DIR/media/local/Backup--timeshift"
+    remoteHomelabBackupDir="$BASE_DIR/homelab/media/local/backup"
+
+    printMessage "Source in test mode check: $localBackupDir"
+    printMessage "Dest in test mode check: $remoteHomelabBackupDir"
   fi
 
   rsync "${rsyncArgs[@]}" "$localBackupDir/" "$remoteHomelabBackupDir/"
@@ -166,6 +213,8 @@ syncBackups() {
   printMessage "Sync of local backups with homelab completed successfully."
 }
 
+# Function: selectBackupTask
+# Description: Displays a menu of backup tasks and performs the selected task based on user input.
 selectBackupTask() {
   local servers=("andrew" "bart" "lisa" "laura" "mary" "homelab")
 
@@ -224,4 +273,3 @@ main() {
 time main "$@"
 
 exit 0
-
